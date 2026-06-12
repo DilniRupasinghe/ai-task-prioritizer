21b32ff26e54c4cd48fe0eb642f8dee36be9a771
DilniRupasinghe <dilni.20230180@iit.ac.lk>
2026-06-12 14:06:24 +0530

    created the funtion for task prioritizer

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+# Request format
+class TaskRequest(BaseModel):
+    tasks: List[str]
 
 @app.get("/")
 def home():
     return {"message": "AI Task Prioritizer API is running"}
+
+@app.post("/prioritize")
+def prioritize_tasks(request: TaskRequest):
+    tasks = request.tasks
+
+    # Simple dummy logic first (NO AI yet)
+    sorted_tasks = sorted(tasks, key=len, reverse=True)
+
+    return {
+        "original_tasks": tasks,
+        "prioritized_tasks": sorted_tasks,
+        "note": "This is placeholder logic (no AI yet)"
+    }
